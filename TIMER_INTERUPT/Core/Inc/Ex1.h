7cdcef41aed6897c41366e7597d124a89ad3aa57
t_k_nguyen-admin <thuan.nguyen26104bk@hcmut.edu.vn>
2024-09-29 12:00:06 +0700

    finishing ex1

---
+++ TIMER_INTERUPT/Core/Inc/Ex1.h
@@ -0,0 +1,13 @@
+/*
+ * Ex1.h
+ *
+ *  Created on: Sep 26, 2024
+ *      Author: thuan
+ */
+
+#ifndef INC_EX1_H_
+#define INC_EX1_H_
+#include "main.h"
+void display7SEG(int num);
+
+#endif /* INC_EX1_H_ */
