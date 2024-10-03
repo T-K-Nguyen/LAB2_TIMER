961c55489817b6cb0f8c31b414b8d5ed7f12c9fe
t_k_nguyen-admin <thuan.nguyen26104bk@hcmut.edu.vn>
2024-10-03 14:15:37 +0700

    finishing Ex4 changing the frequency

---
+++ TIMER_INTERUPT/Core/Inc/Ex4.h
@@ -0,0 +1,19 @@
+/*
+ * Ex4.h
+ *
+ *  Created on: Oct 3, 2024
+ *      Author: thuan
+ */
+
+#ifndef INC_EX4_H_
+#define INC_EX4_H_
+#include "Ex3.h"
+
+#define led_count 100
+#define time_count 100
+void display7SEG(int num);
+
+void update7SEG (int* index );
+
+
+#endif /* INC_EX4_H_ */
